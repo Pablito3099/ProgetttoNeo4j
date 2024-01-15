507aaa05cff2c9c5040b1e0889eec1c88d012ec6
Clodmanzo <c.manzoni@itsrizzoli.it>
2024-01-15 16:36:42 +0100

    agg visual piste e piste aperte

--- menù.py
+++ menù.py
@@ -1,8 +1,10 @@
 from prettytable import PrettyTable
+from neo4j import GraphDatabase
+from colorama import Fore, Style
 
 
 
-#menù iniziale
+# Menù iniziale
 def main_menu():#(connessionedb)
 
     while True:
@@ -22,7 +24,7 @@ def main_menu():#(connessionedb)
         else:
             print("\n--Scelta non valida.--\n")'''
     
-#stampa risultato scelta 2
+# Stampa risultato scelta 2
 def visualizza_tutte_piste():
 
     #info dal db
@@ -45,11 +47,42 @@ def visualizza_tutte_piste():
     else:
         print("\n--Scelta non valida--\n")
 
-
+#Aperto = Verde \ Chiuso = Rosso
+def colora_stato(stato):
+    if stato.lower() == 'aperto':
+        return f"{Fore.GREEN}{stato}{Style.RESET_ALL}"
+    elif stato.lower() == 'chiuso':
+        return f"{Fore.RED}{stato}{Style.RESET_ALL}"
+    else:
+        return stato
+    
+# Stampa risultato scelta 3
 def visual_piste_aperte():
-    
 
-    
+    '''
+    #info dal db
+    query =
+    result = session.run(query)
+    '''
+
+    #creazione tabella e aggiunta risultati
+    table = PrettyTable()
+    table.field_names = ["Nome Pista", "Difficoltà", "Stato"]
+
+    for record in result:
+         table.add_row([record["Nome"], record["Difficolta"], colora_stato(record["Stato"])])
+
+    #Visualizzazione
+    print(table)
+    input(print("\n1. Indietro\n-"))
+
+    if input == "1":
+        main_menu()
+    else:
+        print("\n--Scelta non valida--\n")
+
+
+
 
 
 if __name__ == "__main__":
